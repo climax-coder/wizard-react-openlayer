f29851929606332e4b710deba2cfa10728aeb5f1
seniorprodev <seniorprodev@gmail.com>
2024-02-21 09:35:26 +0100

    complete stepper and fix navbar link issue on mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
+import Wizard from '../components/ProjectWizard/Wizard';
 
 const Home = () => {
   return (
     <Box mt={8} mx="auto" maxW="1280px">
-       Home Page
+       <Wizard />
     </Box>
   );
 };
