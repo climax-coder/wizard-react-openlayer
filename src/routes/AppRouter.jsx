d9fe83599a42c7aaf51726212472552256303e4a
seniorprodev <seniorprodev@gmail.com>
2024-02-21 06:58:06 +0100

    configure app routes

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
+import Projects from "../pages/Projects";
+import ProjectDetails from "../pages/ProjectDetails";
+import NotFound from "../pages/NotFound";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" exact Component={Home} />
+      <Route path="/dashboard" exact Component={Dashboard} />
+      <Route path="/projects" exact Component={Projects} />
+      <Route path="/projects/:id" Component={ProjectDetails} />
+      <Route path="/create" Component={CreateProject} />
+      <Route path="*" Component={NotFound} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
